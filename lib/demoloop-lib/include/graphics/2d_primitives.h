ad6922bcf6f9c855b7cab268a0fa7447fa10bf7d
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-10-19 19:03:30 -0400

    render triangle

--- lib/demoloop-lib/include/graphics/2d_primitives.h
+++ lib/demoloop-lib/include/graphics/2d_primitives.h
@@ -7,6 +7,7 @@ namespace Demoloop {
 void ellipse(GL& gl, const float x, const float y, const float radiusX, const float radiusY, int points = 10);
 void circle(GL& gl, const float x, const float y, const float radius, int points = 10);
 void rectangle(GL& gl, const float x, const float y, const float w, const float h);
+void triangle(GL& gl, const float x1, const float y1, const float x2, const float y2, const float x3, const float y3);
 void polygon(GL& gl, const float* xCoords, const float* yCoords, uint32_t count);
 
 }
--- lib/demoloop-lib/src/graphics/2d_primitives.cpp
+++ lib/demoloop-lib/src/graphics/2d_primitives.cpp
@@ -30,6 +30,12 @@ void rectangle(GL& gl, const float x, const float y, const float w, const float 
   polygon(gl, xCoords, yCoords, 4);
 }
 
+void triangle(GL& gl, const float x1, const float y1, const float x2, const float y2, const float x3, const float y3) {
+  const float xCoords[] = {x1, x2, x3};
+  const float yCoords[] = {y1, y2, y3};
+  polygon(gl, xCoords, yCoords, 3);
+}
+
 void polygon(GL& gl, const float* xCoords, const float* yCoords, uint32_t count) {
   const float x = xCoords[0];
   const float y = yCoords[0];
