855f4ca50c73f43019e01c26278eaec3e7095740
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-10-19 19:19:34 -0400

    loop4

--- src/loop004.cpp
+++ src/loop004.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL2_gfxPrimitives.h>
-#include "demoloop.h"
-#include "helpers.h"
+#include "demoloop_opengl.h"
+#include "graphics/2d_primitives.h"
 #include "hsl.h"
 using namespace std;
 
@@ -10,9 +8,11 @@ float t = 0;
 const float PI = 3.1459;
 const float CYCLE_LENGTH = 10;
 
-class Loop4 : public Demoloop {
+class Loop4 : public Demoloop::DemoloopOpenGL {
 public:
-  Loop4() : Demoloop(150, 150, 150) {}
+  Loop4() : Demoloop::DemoloopOpenGL(150, 150, 150) {
+    glDisable(GL_DEPTH_TEST);
+  }
 
   void Update(float dt) {
     t += dt;
@@ -25,8 +25,8 @@ public:
 
     const int num_vertices = 8;
     const float interval = (PI * 2) / num_vertices;
-    int16_t xCoords[num_vertices];
-    int16_t yCoords[num_vertices];
+    float xCoords[num_vertices];
+    float yCoords[num_vertices];
     for (int i = 0; i < num_vertices; ++i) {
       float t = i;
       xCoords[i] = cos(interval * t) * RADIUS + ox;
@@ -34,7 +34,8 @@ public:
     }
 
     auto color = hsl2rgb(cycle_ratio, 1, 0.5);
-    filledPolygonColor(renderer, xCoords, yCoords, num_vertices, rgb2uint32(color));
+    setColor(color);
+    polygon(gl, xCoords, yCoords, num_vertices);
 
     const int dot_count = 20;
     for (float i = 0; i < dot_count; ++i) {
@@ -49,7 +50,8 @@ public:
       x1 = c * x1 - s * y1;
       y1 = s * x1 + c * y1;
 
-      filledCircleRGBA(renderer, x1 + ox, y1 + oy, 3, 0, 0, 0, 255);
+      setColor(0, 0, 0);
+      circle(gl, x1 + ox, y1 + oy, 3);
     }
   }
 
