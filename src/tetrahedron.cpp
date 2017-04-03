c0e8fd9e63ed6f9510a46efe338fcfafc609b14d
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-04-02 21:51:46 -0400

    fully stacked

--- src/tetrahedron.cpp
+++ src/tetrahedron.cpp
@@ -3,8 +3,6 @@
 #include <glm/gtx/rotate_vector.hpp>
 #include "math_helpers.h"
 #include <array>
-#include <algorithm>
-#include <functional>
 using namespace std;
 using namespace demoloop;
 
@@ -21,8 +19,8 @@ array<Vertex, NUM_INDICES> toNonIndexed(const array<Vertex, NUM_VERTS> &in_verts
   return out;
 }
 
-
 const float radius = 3;
+// Tetrahedron
 const array<Vertex, 4> indexedVertices = {{
   {1,  1,  1},
   {-1, -1,  1},
@@ -37,15 +35,68 @@ const array<uint32_t, 12> indices = {{
   2,  3,  1
 }};
 
-void subdivideFace(Vertex *out, uint32_t &index, const Vertex &a, const Vertex &b, const Vertex &c, uint32_t detail) {
+// Octahedron
+// const array<Vertex, 6> indexedVertices = {{
+//   {1, 0, 0},   {- 1, 0, 0},   { 0, 1, 0},   { 0, - 1, 0},   { 0, 0, 1},   { 0, 0, - 1}
+// }};
+
+// const array<uint32_t, 8 * 3> indices = {{
+//   0, 2, 4,    0, 4, 3,    0, 3, 5,    0, 5, 2,    1, 2, 5,    1, 5, 3,    1, 3, 4,    1, 4, 2
+// }};
+
+// Dodecahedron
+// const float p = ( 1 + sqrt( 5 ) ) / 2;
+// const float r = 1 / p;
+// const array<Vertex, 20> indexedVertices = {{
+//   // (±1, ±1, ±1)
+//   {- 1, - 1, - 1},    {- 1, - 1,  1},
+//   {- 1,  1, - 1},    {- 1,  1,  1},
+//    {1, - 1, - 1},    { 1, - 1,  1},
+//    {1,  1, - 1},    { 1,  1,  1},
+
+//   // (0, ±1/φ, ±φ)
+//    {0, - r, - p},     {0, - r,  p},
+//    {0,  r, - p},     {0,  r,  p},
+
+//   // (±1/φ, ±φ, 0)
+//   {- r, - p,  0},    {- r,  p,  0},
+//    {r, - p,  0},     {r,  p,  0},
+
+//   // (±φ, 0, ±1/φ)
+//   {- p,  0, - r},     {p,  0, - r},
+//   {- p,  0,  r},     {p,  0,  r}
+// }};
+
+// const array<uint32_t, 108> indices = {{
+//    3, 11,  7,      3,  7, 15,      3, 15, 13,
+//    7, 19, 17,      7, 17,  6,      7,  6, 15,
+//   17,  4,  8,     17,  8, 10,     17, 10,  6,
+//    8,  0, 16,      8, 16,  2,      8,  2, 10,
+//    0, 12,  1,      0,  1, 18,      0, 18, 16,
+//    6, 10,  2,      6,  2, 13,      6, 13, 15,
+//    2, 16, 18,      2, 18,  3,      2,  3, 13,
+//   18,  1,  9,     18,  9, 11,     18, 11,  3,
+//    4, 14, 12,      4, 12,  0,      4,  0,  8,
+//   11,  9,  5,     11,  5, 19,     11, 19,  7,
+//   19,  5, 14,     19, 14,  4,     19,  4, 17,
+//    1, 12, 14,      1, 14,  5,      1,  5,  9
+// }};
+
+template<class T>
+constexpr T pow(const T base, unsigned const exponent) {
+    // (parentheses not required in next line)
+    return (exponent == 0)     ? 1 :
+           (exponent % 2 == 0) ? pow(base, exponent/2)*pow(base, exponent/2) :
+           base * pow(base, (exponent-1)/2) * pow(base, (exponent-1)/2);
+}
+
+template<uint32_t detail>
+void subdivideFace(Vertex *out, uint32_t &index, const Vertex &a, const Vertex &b, const Vertex &c) {
   const uint32_t cols = pow(2, detail);
-  vector<vector<Vertex>> v;
-  v.resize(cols + 1);
+  array<array<Vertex, cols + 1>, cols + 1> v;
 
   for (uint32_t i = 0; i <= cols; ++i) {
     const uint32_t rows = cols - i;
-
-    v[i].resize(rows + 1);
 
     Vertex aj = mix(a, c, static_cast<float>(i) / cols);
     Vertex bj = mix(b, c, static_cast<float>(i) / cols);
@@ -76,35 +127,26 @@ void subdivideFace(Vertex *out, uint32_t &index, const Vertex &a, const Vertex &
   }
 }
 
-template<class T>
-constexpr T pow(const T base, unsigned const exponent) {
-    // (parentheses not required in next line)
-    return (exponent == 0)     ? 1 :
-           (exponent % 2 == 0) ? pow(base, exponent/2)*pow(base, exponent/2) :
-           base * pow(base, (exponent-1)/2) * pow(base, (exponent-1)/2);
-}
-
 template<
   uint32_t DETAIL = 0,
   size_t NUM_VERTS,
-  size_t NUM_INDICES,
-  typename = typename std::enable_if<DETAIL >= 0>::type>
+  size_t NUM_INDICES>
 array<Vertex, NUM_INDICES * pow<uint32_t>(4, DETAIL)> subdivide(const array<Vertex, NUM_VERTS> &in, const array<uint32_t, NUM_INDICES> &indices) {
   array<Vertex, NUM_INDICES * pow<uint32_t>(4, DETAIL)> out;
-
   uint32_t index = 0;
+
   for (uint32_t i = 0; i < indices.size(); i+=3) {
     const Vertex &a = in[indices[i + 0]];
     const Vertex &b = in[indices[i + 1]];
     const Vertex &c = in[indices[i + 2]];
 
-    subdivideFace(out.data(), index, a, b, c, DETAIL);
+    subdivideFace<DETAIL>(out.data(), index, a, b, c);
   }
 
   return out;
 }
 
-const auto vertices = subdivide<4>(indexedVertices, indices);
+const auto vertices = subdivide<2>(indexedVertices, indices);
 
 class Loop037 : public Demoloop {
 public:
